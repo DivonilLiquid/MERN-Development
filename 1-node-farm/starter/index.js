dbe761fc09e1830570a6a94438355caa2dc90a87
DivonilLiquid <mait.madhur@gmail.com>
2021-05-29 16:06:38 +0530

    API Build

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 /*          Files                */
 /*Blocking,synchronous ways  */
 /*File system module - fs */
@@ -27,9 +28,37 @@ const http = require('http');
 
 
 /*          Server           */
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObject = JSON.parse(data);
 const server = http.createServer((req,res)=>{
-    console.log(req);
-    res.end('Hello from the server');
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('Hello from the overview');
+    }
+    else if(pathName === '/product'){
+        res.end('Hello from product');
+    }
+    else if(pathName==='/api'){
+        res.writeHead(200,{'Content-type':'application/json'});    
+        res.end(data);
+        // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>{
+        //     const productData = JSON.parse(data);
+        //     res.writeHead(200,{'Content-type':'application/json'});
+        //     //console.log(productData);
+        //     res.end(data);
+        // });
+
+        //res.end("API");
+    }
+    else{
+        res.writeHead(404,{
+            'Content-type':'text/html',
+            'my-own-header':'hello-world'
+            /*These are Response headers, use to send some metadata about response itself */
+        });
+        res.end('<h1>404: page not found</h1>');
+    }
+    
 });
 server.listen(8000,'127.0.0.1',()=>{
     console.log("Listening to request on port 8000");
