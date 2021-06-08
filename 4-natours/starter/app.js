99736f63cea551269a01fbb6700d874013aa6221
DivonilLiquid <mait.madhur@gmail.com>
2021-06-08 19:36:39 +0530

    Routing + Middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const fs = require('fs');
-
+const morgan = require('morgan');
 
 const app = express();                      //express() is a function which on calling adds bunch of method in app
-
-app.use(express.json());                    //middleware used to modify incoming responses
 
 const port = 3000;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 
+// 1) Middleware
+
+app.use(morgan('dev'));   //Concise output colored by response status for development use. The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes. 
+
+app.use(express.json());                    //middleware used to modify incoming responses
+
+app.use((req, res, next)=>{
+    console.log('Hello from own middleware 👋🏻');
+    req.requestTime = new Date().toISOString(); // new Date() is a constructor
+    next(); //very important to implement next stacked middlewares
+})
+
 app.get('/', (req, res) =>{                 //http method get used to get the infromation
     res.status(200).send('Hello response from server');
 })
+
+// 2) Route Handlers 
 
 const getTours = (req, res) =>{                 //http method get used to get the infromation
     res.status(200)
     .json(
         {
             status: 'success', 
+            requestTime: req.requestTime,
             results: tours.length,
             data: {tours}
         }
@@ -107,6 +120,38 @@ const deleteTour = (req, res) =>{
     );
 }
 
+const getUsers = (req, res) =>{
+    res.status(500).send({
+        status: 'fail',
+        message: 'server not defined'
+    })
+}
+const addUser = (req, res) =>{
+    res.status(500).send({
+        status: 'fail',
+        message: 'server not defined'
+    })
+}
+const getUser = (req, res) =>{
+    res.status(500).send({
+        status: 'fail',
+        message: 'server not defined'
+    })
+}
+const updateUser = (req, res) =>{
+    res.status(500).send({
+        status: 'fail',
+        message: 'server not defined'
+    })
+}
+const deleteUser = (req, res) =>{
+    res.status(500).send({
+        status: 'fail',
+        message: 'server not defined'
+    })
+}
+
+
 // app.get('/api/v1/tours', getTours);
 
 // app.post('/api/v1/tours', addTour);
@@ -118,14 +163,21 @@ const deleteTour = (req, res) =>{
 
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
 
+//data
 app.route('/api/v1/tours').get(getTours).post(addTour);
 
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
+//Users
+app.route('/api/v1/users').get(getUsers).post(addUser);
+
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 
 
+// 4) Server start
 app.listen(port,()=>{                       //used to start the server
     console.log('Hello from server side');
 })
