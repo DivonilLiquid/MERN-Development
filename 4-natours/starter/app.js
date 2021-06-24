e69c3d49e9884bed05cfea0bb079fface400099e
DivonilLiquid <mait.madhur@gmail.com>
2021-06-24 22:29:57 +0530

    Better Errors and Refactoring

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express(); //express() is a function which on calling adds bunch of method in app
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRoute = require('./route/tourRouter');
 const userRoute = require('./route/userRouter');
 
@@ -14,12 +15,6 @@ app.use(express.json()); //middleware used to modify incoming responses
 
 app.use(express.static(`${__dirname}/public`)); //static file path where url will be -> http://127.0.0.1:3000/overview.html
 
-app.use((req, res, next) => {
-  console.log('Hello from own middleware 👋🏻');
-  req.requestTime = new Date().toISOString(); // new Date() is a constructor
-  next(); //very important to implement next stacked middlewares
-});
-
 app.get('/', (req, res) => {
   //http method get used to get the infromation
   res.status(200).send('Hello response from server');
@@ -29,25 +24,10 @@ app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
 //if no route is defined till here, then it the route defined below will get executed.
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on server`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on server`);
-  //error message has been initialised
-  err.statusCode = 404;
-  err.status = 'fail';
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on server`, 404));
 });
 
 //global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: `Can't find ${req.originalUrl} on server`,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
